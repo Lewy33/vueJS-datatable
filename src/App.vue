3ea6b34c97647ad43aed215fdf58cc45f9076d3b
Aikoze <youenn.toullec@gmail.com>
2018-02-26 13:20:35 +0100

    Ajout des suppressions de lignes

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <Liste msg="Welcome to Your Vue.js App"/>
-
+    <Liste></Liste>
   </div>
 </template>
 
--- src/components/Liste.vue
+++ src/components/Liste.vue
@@ -1,6 +1,17 @@
 <template>
     <div class="liste">
-        <form method="post" action="#">
+        <button @click="deleteInter()">
+            Supprimer
+        </button>
+        <button @click="addRow()">Ajouter une inter</button>
+        <!--<form method="post" action="#">-->
+            <div id="nvlleInter" style="display: none">
+                <input v-model="rows.firstname" placeholder="firstname"/>
+                <input v-model="rows.lastname" placeholder="lastname"/>
+                <input v-model="rows.titre" placeholder="titre"/>
+                <textarea v-model="rows.description" placeholder="description"/>
+                <input type="submit" @click="addInter"/>
+            </div>
             <table>
                 <thead>
                     <tr>
@@ -12,23 +23,21 @@
                             {{column}}
 
                         </th>
-                        <th @click="deleteInter(index)">
-                            Actions
-                        </th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr v-for="inter in dataByKeyAndOrder">
-                        <td>{{inter.id}}</td>
-                        <td>{{inter.first_name}}</td>
-                        <td>{{inter.last_name}}</td>
-                        <td>{{inter.titre}}</td>
-                        <td>{{inter.description}}</td>
-                        <td><input type="checkbox"/></td>
+                    <tr v-for="(inter,index) in dataByKeyAndOrder">
+                        <td v-model="rows.id">{{inter.id}}</td>
+                        <td v-model="rows.firstname">{{inter.first_name}}</td>
+                        <td v-model="rows.lastname">{{inter.last_name}}</td>
+                        <td v-model="rows.titre">{{inter.titre}}</td>
+                        <td v-model="rows.description">{{inter.description}}</td>
+                        <td><input type="checkbox" @click="deleteMe(inter.id)" :checked="toDelete.indexOf(inter.id) > -1 " /></td>
                     </tr>
                 </tbody>
+
             </table>
-        </form>
+        <!--</form>-->
     </div>
 </template>
 
@@ -41,16 +50,21 @@
             data: Array,
             filterKey: String
         },
-        data(){
+        data() {
             return {
-                interventions:[],
+                interventions: [],
                 columns: [],
                 order: {
                     by: 'id',
                     order: 'ASC'
+
+                },
+                toDelete: [],
+                rows: {
+                    id:'', firstname:'',lastname:'',titre :'',description:''
                 }
-            }
-        },
+                }
+            },
         computed: {
             dataByKeyAndOrder(){
                 var compare = function (filter) {
@@ -83,6 +97,7 @@
                 axios.get(`https://raw.githubusercontent.com/mdubourg001/datatable_vuejs/master/src/assets/REDUCED_DATA.json`)
                     .then(response => {
                         // JSON responses are automatically parsed.
+                        console.log(response.data)
                         this.interventions = response.data;
                         this.columns = Object.keys(response.data[0])
                     })
@@ -91,8 +106,18 @@
                     })
             },
             deleteInter(index){
-                var data =  this.interventions
-                data.splice(index, 1)
+                console.log(this.toDelete)
+                for(let i = 0 ; i<this.toDelete.length; i++){
+                    for(let j=0; j<this.interventions.length; j++){
+                        if(this.interventions[j].id == this.toDelete[i]){
+                            this.interventions.splice(j, 1)
+                        }
+                    }
+                }
+                this.toDelete = []
+            },
+            deleteMe(id){
+                this.toDelete.push(id)
             },
             sortByKey(key){
                 if(key !== this.order.by) {
@@ -103,6 +128,17 @@
                 }else {
                     this.order.order = 'ASC'
                 }
+            },
+            addRow() {
+                var divAdd = document.getElementById('nvlleInter')
+                if (divAdd.style.display == 'none') {
+                    divAdd.style.display = 'block';
+                } else {
+                    divAdd.style.display = 'none';
+                }
+            },
+            addInter(){
+                this.rows.push()
             }
         },
         mounted() {
